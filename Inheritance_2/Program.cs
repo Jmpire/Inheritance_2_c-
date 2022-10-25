3a2984ab0b03ede447ec4c84fa4375afa8b110bd
Malakia Jeremia <malakiajerr2@gmail.com>
2022-10-25 22:58:41 +0200

    Add project files.

---
+++ Inheritance_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Inheritance_2
+{
+    class Program
+    {
+        // Inheritance and Polymorphism( virtual, abstract, interfaces)
+        abstract class Shape
+        {
+            public abstract void Type();
+
+        }
+        class Circle:Shape
+        {
+            public override void Type()
+            {
+                Console.WriteLine("This is a circle");
+            }
+        }
+        class Rect:Shape
+        {
+            public override void Type()
+            {
+                Console.WriteLine("This is a rectangle");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Shape rect = new Rect();
+            Shape circle = new Circle();
+            rect.Type();
+            circle.Type();
+        }
+    }
+}
